99e5bb053fd12ae32df0891960c903b007a90b97
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-29 01:05:22 -0300

    feat: page home

--- brStore/src/pages/Home/index.js
+++ brStore/src/pages/Home/index.js
@@ -1,10 +1,29 @@
-import './style.css';
+import ProductCard from '../../components/ProductCard';
+import ProductList from '../../components/ProductList/ProductList';
+import { allProducts } from '../../Database/database';
 
 const Home = () => {
     return (
         <main className='w-screen p-2'>
-            
-            <h1 className='w-11/12 mx-auto'>Index</h1>
+
+            <ProductList nomeLista={'Nossos produtos'}>
+
+                {
+                    allProducts.map(product => (
+                        
+                        <ProductCard
+                            nome={product.nomeProd}
+                            img={product.imagem}
+                            precoAnterior={product.precoAntigo.toFixed(2)}
+                            precoAtual={product.precoNovo.toFixed(2)}
+                            key={product.id}
+                        />
+
+                    ))
+
+                }
+
+            </ProductList>
 
         </main>
     );
