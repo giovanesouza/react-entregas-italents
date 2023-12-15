e55cfa62cedc682c20be118af6e8db6b00d87c8b
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-14 21:31:28 -0300

    refactor: lista de produtos add com useState

--- brStore/src/pages/Checkout/index.js
+++ brStore/src/pages/Checkout/index.js
@@ -1,14 +1,13 @@
 import { SectionCheckout } from "../../components/SectionCheckout";
 import { Table } from "../../components/SectionCheckout/table";
 import { Count } from "../../components/SectionCheckout/count";
-import camera from '../../assets/images/products/eletronicos/camera-canon.jpg';
+import { useState } from "react";
 
 const Checkout = () => {
 
-    const items = [
-        { id: 1, nome: 'Câmera Canon', imagem: camera, precoUnit: 15.99, qtd: 1, precoTotal: 15.99 },
-    ];
-
+    // Items add ao carrinho
+    const [itemsBag, setItemsBag] = useState([
+    ]);
 
     return (
         <main className="w-screen p-2">
@@ -18,26 +17,26 @@ const Checkout = () => {
                 <Table>
 
                     {
-                        items.length === 0 ?
+                        itemsBag.length === 0 ?
                             (
                                 <tr className="text-base text-gray-600 text-center">
                                     <td className="p-3" td colSpan={5}>Sua sacola está vazia</td>
                                 </tr>
-                    )
-                    :
-                            items.map((item, index) => (
-                    <tr className="text-gray-600" key={index}>
-                        <td className="flex items-center gap-2 py-2"><img src={item.imagem} alt={item.nome} className='w-16' /> {item.nome}</td>
-                        <td className="text-center">R$ {item.precoUnit}</td>
-                        <td className="text-center">
-                            <Count qtd={item.qtd} />
-                        </td>
-                        <td className="text-center">R$ {item.precoTotal}</td>
-                        <td className="text-center text-red-600">
-                            <i className="bi bi-trash3-fill cursor-pointer"></i>
-                        </td>
-                    </tr>
-                    ))
+                            )
+                            :
+                            itemsBag.map((item, index) => (
+                                <tr className="text-gray-600" key={index}>
+                                    <td className="flex items-center gap-2 py-2"><img src={item.imagem} alt={item.nome} className='w-16' /> {item.nome}</td>
+                                    <td className="text-center">R$ {item.precoUnit}</td>
+                                    <td className="text-center">
+                                        <Count qtd={item.qtd} />
+                                    </td>
+                                    <td className="text-center">R$ {item.precoTotal}</td>
+                                    <td className="text-center text-red-600">
+                                        <i className="bi bi-trash3-fill cursor-pointer"></i>
+                                    </td>
+                                </tr>
+                            ))
                     }
 
                 </Table>
