12b4b3405924b5a690e133b2fbf004881b99839e
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-19 18:37:09 -0300

    feat: busca do usuário ao logar

--- brStore/src/pages/Home/index.js
+++ brStore/src/pages/Home/index.js
@@ -1,8 +1,28 @@
+import { useContext, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard';
 import ProductList from '../../components/ProductList/ProductList';
 import { useStoreAPI } from '../../hooks/useStoreAPI';
+import { AuthContext } from '../../context/AuthContext';
+
 
 const Home = () => {
+
+    const { findUserById } = useContext(AuthContext);
+
+    useEffect(() => {
+        console.log('COMPONENTE NASCEU NA HOME');
+
+        const userInfo = JSON.parse(localStorage.getItem('userInfo')); // contem email, id, token
+        // Verifica se existe...
+
+        // Pega as informações do usuário com base no id
+        if (userInfo) {
+            // Busca o usuário pelo id e seta as informações dele em userFull e no localStorage
+            findUserById(userInfo.id);
+        };
+
+
+    }, []);
 
     const allProducts = useStoreAPI();
 
@@ -13,7 +33,7 @@ const Home = () => {
 
                 {
                     allProducts.map(product => (
-                      
+
                         <ProductCard
                             product={product}
                             key={product.id}
