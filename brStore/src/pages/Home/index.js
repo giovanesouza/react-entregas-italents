22401beea6799e322095a8c121b9d96e2ab340aa
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-19 21:25:54 -0300

    feat: listagem de produtos de acordo com o usuário logado (admin/client)

--- brStore/src/pages/Home/index.js
+++ brStore/src/pages/Home/index.js
@@ -1,16 +1,29 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ProductCard from '../../components/ProductCard';
 import ProductList from '../../components/ProductList/ProductList';
-import { useStoreAPI } from '../../hooks/useStoreAPI';
 import { AuthContext } from '../../context/AuthContext';
+import { findAllProducts, deleteProduct } from '../../services/productService';
+import { Link, useNavigate } from 'react-router-dom';
+import Admin from '../Admin/Products';
+import ProductListAdmin from '../../components/ProductList/ProductListAdmin';
 
 
 const Home = () => {
 
-    const { findUserById } = useContext(AuthContext);
+    const { findUserById, isAdmin } = useContext(AuthContext);
+
+
+    // Pega todos os produtos cadastrados no BD
+    const [products, setProducts] = useState([]);
+
+    const navigate = useNavigate();
+
+
 
     useEffect(() => {
         console.log('COMPONENTE NASCEU NA HOME');
+
+        getAllProducts(); // Lista todos os produtos do BD
 
         const userInfo = JSON.parse(localStorage.getItem('userInfo')); // contem email, id, token
         // Verifica se existe...
@@ -24,25 +37,70 @@ const Home = () => {
 
     }, []);
 
-    const allProducts = useStoreAPI();
+
+    const getAllProducts = async () => {
+        try {
+            const response = await findAllProducts();
+            console.log(response)
+
+            setProducts(response.data);
+
+        } catch (error) {
+            const err = error.response.data.message;
+            console.error('Erro findById (front): ', err);
+        }
+
+    };
+
+
+
+    const removeProduct = async (id) => {
+        const answer = window.confirm('Deseja excluir o produto?');
+        if (answer) {
+            await deleteProduct(id);
+            getAllProducts();
+        }
+    };
+
 
     return (
         <main className='w-screen p-2'>
 
-            <ProductList nomeLista={'Nossos produtos'}>
+            {
+                isAdmin ?
 
-                {
-                    allProducts.map(product => (
+                    (
+                        // Lista de produtos cadastrados com opção de adicionar, editar  e excluir
+                        <Admin>
+                            {products.map(product => (
 
-                        <ProductCard
-                            product={product}
-                            key={product.id}
-                        />
+                                <ProductListAdmin product={product}  key={product._id} onClick={() => removeProduct(product._id)} />
 
-                    ))
-                }
+                            ))}
 
-            </ProductList>
+                        </Admin>
+                    )
+
+                    :
+
+                    (
+                        // Listagem de produtos para os clientes
+                        <ProductList nomeLista={'Nossos produtos'}>
+                            {
+                                products.map(product => (
+
+                                    <ProductCard
+                                        product={product}
+                                        key={product.id}
+                                    />
+
+                                ))
+                            }
+                        </ProductList>
+                    )
+
+            }
+
 
         </main>
     );
