e6ec5fc981817d0e1a9ef5aef53a82cea9ae1693
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-21 22:57:20 -0300

    exclusão de arquivos dispensáveis para a aplicação

--- brStore/src/components/SectionCheckout/Aside/Price.js
+++
@@ -1,8 +0,0 @@
-export const Price = ({ labelInfo, labelValue }) => {
-    return (
-        <div className="w-full flex justify-between items-center text-gray-600 p-2">
-            <span>{labelInfo}</span>
-            <span>R$ {labelValue.toFixed(2)}</span>
-        </div>
-    );
-}
