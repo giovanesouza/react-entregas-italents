8acd1ab27a3d9e36969e64055b3aa40bf6b6800e
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-24 16:23:27 -0300

    feat: add e remoção de produtos favoritos

--- brStore/src/services/userService.js
+++ brStore/src/services/userService.js
@@ -9,5 +9,22 @@ const createUser = (user) =>
         }); // Erro
 
 
+const addFavProduct = (userId, productId) =>
+    api.post(`/usuario/addFavProduct/${userId}`, productId)
+        .then((response) => response)
+        .catch(error => {
+            console.error('Erro (back): ', error);
+            throw error; // Lança o erro e permite pegá-lo no front
+        }); // Erro
 
-export { createUser }
+
+const removeFavProduct = (userId, productId) =>
+    api.delete(`/usuario/removeFavProduct/${userId}`, productId)
+        .then((response) => response)
+        .catch(error => {
+            console.error('Erro (back): ', error);
+            throw error; // Lança o erro e permite pegá-lo no front
+        }); // Erro
+
+
+export { createUser, addFavProduct, removeFavProduct };
